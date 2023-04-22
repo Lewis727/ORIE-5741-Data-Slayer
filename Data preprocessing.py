632bb0edbd8a2bc6c776f4a85bbfa6e3b2e17548
littledylanding <58164353+littledylanding@users.noreply.github.com>
2023-04-22 14:10:52 -0400

    data update

--- Data preprocessing.py
+++ Data preprocessing.py
@@ -1,7 +1,19 @@
 import pandas as pd
+import os
 
-data = pd.read_excel('Region_NW.xlsx', index_col=1)
+path = os.path.realpath(__file__)
+dir = os.path.dirname(path)
+dir += '\Data\\'
+data = pd.read_excel(dir + 'Region_NW.xlsx', index_col=1)
 data.index = pd.to_datetime(data.index)
-data = data.dropna(axis='columns', how='all')
-data = data.dropna(axis=0, subset=['D'])
 data = data.drop(columns=['Local time', 'Local date', 'Hour', 'Time zone', 'DF', 'Region'])
+
+regions = ['AVA', 'AVRN', 'BPAT', 'CHPD', 'DOPD', 'GCPD', 'GRID', 'GWA', 'IPCO', 'NEVP', 'NWMT', 'PACE', 'PACW', 'PGE', 'PSCO', 'PSEI', 'SCL', 'TPWR', 'WACM', 'WAUW', 'WWA']
+for r in regions:
+    region = pd.read_excel(dir + r + '.xlsx', index_col=1)
+    region.index = pd.to_datetime(region.index)
+    temp = region['D']
+    data = data.join(temp, rsuffix='_'+r)
+data = data.dropna(axis='columns', how='all')
+data = data.dropna()
+data.to_csv(dir+'data.csv')
---
+++ test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import os
+
+path = os.path.realpath(__file__)
+dir = os.path.dirname(path)
+dir += '\Data\\'
+data = pd.read_csv(dir + 'data.csv', index_col=0)
+data.index = pd.to_datetime(data.index)
+print(data)
