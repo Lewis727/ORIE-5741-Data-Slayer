984385d689bfd8ca8a32e46c136b618993533986
jd2269 <dylanjiaqi@gmail.com>
2023-05-08 10:49:35 -0400

    autoencoder update

--- autoencoder.py
+++ autoencoder.py
@@ -16,7 +16,7 @@ input_dim = x_train.shape[1]
 encoding_dim = 3
 input_layer = Input(shape=(input_dim,))
 encoder = Dense(encoding_dim, activation='relu')(input_layer)
-decoder = Dense(input_dim, activation='sigmoid')(encoder)
+decoder = Dense(input_dim, activation='tanh')(encoder)
 
 # Define the autoencoder model
 autoencoder = Model(inputs=input_layer, outputs=decoder)
@@ -25,7 +25,7 @@ autoencoder = Model(inputs=input_layer, outputs=decoder)
 autoencoder.compile(optimizer='adam', loss='mse')
 
 # Train the autoencoder on the mixed dataset
-autoencoder.fit(x_train, x_train, epochs=50, batch_size=32)
+autoencoder.fit(x_train, x_train, epochs=100, batch_size=32)
 
 # Use the trained autoencoder to reconstruct the mixed dataset
 reconstructions = autoencoder.predict(x_train)
@@ -47,15 +47,3 @@ ax.scatter(anomalies.index, anomalies['D'], color='red', label='Anomalies')
 
 plt.legend()
 plt.savefig('autoencoder.jpg')
-"""
-# Fine-tune the autoencoder on the normal data only (optional)
-autoencoder.fit(x_normal, x_normal, epochs=10, batch_size=32)
-
-# Use the trained autoencoder to detect anomalies in new, unseen data
-x_test = ...
-reconstructions = autoencoder.predict(x_test)
-mse = tf.keras.losses.mean_squared_error(x_test, reconstructions)
-mse = mse.numpy()
-is_anomaly = mse > threshold
-x_anomaly = x_test[is_anomaly]
-"""
